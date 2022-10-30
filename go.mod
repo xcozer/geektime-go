4a302b4a234d94d3f17db86bcf4780e3c2b93e96
Deng Ming <mingflycash@gmail.com>
2022-10-30 19:09:29 +0800

    静态路由匹配

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/kataras/iris/v12 v12.1.8
 	github.com/labstack/echo-contrib v0.13.0
 	github.com/labstack/echo/v4 v4.9.1
+	github.com/stretchr/testify v1.8.0
 )
 
 require (
@@ -21,6 +22,7 @@ require (
 	github.com/aymerick/raymond v2.0.3-0.20180322193309-b565731e1464+incompatible // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/eknkc/amber v0.0.0-20171010120322-cdade1c07385 // indirect
 	github.com/fatih/structs v1.1.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
@@ -56,6 +58,7 @@ require (
 	github.com/moul/http2curl v1.0.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_golang v1.12.2 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.37.0 // indirect
---
+++ web/router.go
@@ -0,0 +1,146 @@
+package web
+
+import (
+	"fmt"
+	"strings"
+)
+
+// 用来支持对路由树的操作
+// 代表路由树（森林）
+type router struct {
+	// Beego Gin HTTP method 对应一棵树
+	// GET 有一棵树，POST 也有一棵树
+
+	// http method => 路由树根节点
+	trees map[string]*node
+}
+
+func newRouter() router {
+	return router{
+		trees: map[string]*node{},
+	}
+}
+
+// 加一些限制：
+// path 必须以 / 开头，不能以 / 结尾，中间也不能有连续的 //
+func (r *router) addRoute(method string, path string, handleFunc HandleFunc) {
+	if path == "" {
+		panic("web: 路径不能为空字符串")
+	}
+	// 首先找到树来
+	root, ok := r.trees[method]
+
+	if !ok {
+		// 说明还没有根节点
+		root = &node{
+			path: "/",
+		}
+		r.trees[method] = root
+	}
+
+	// 开头不能没有/
+	if path[0] != '/' {
+		panic("web: 路径必须以 / 开头")
+	}
+
+	// 结尾
+	if path != "/" && path[len(path)-1] == '/' {
+		panic("web: 路径不能以 / 结尾")
+	}
+
+	// 中间连续 //，可以用 strings.contains("//")
+
+	// 根节点特殊处理一下
+	if path == "/" {
+		// 根节点重复注册
+		if root.handler != nil {
+			panic("web: 路由冲突，重复注册[/]")
+		}
+		root.handler = handleFunc
+		return
+	}
+
+	// /user/home 被切割成三段
+	// 切割这个 path
+	segs := strings.Split(path[1:], "/")
+	for _, seg := range segs {
+		if seg == "" {
+			panic("web: 不能有连续的 /")
+		}
+		// 递归下去，找准位置
+		// 如果中途有节点不存在，你就要创建出来
+		child := root.childOrCreate(seg)
+		root = child
+	}
+	if root.handler != nil {
+		panic(fmt.Sprintf("web: 路由冲突，重复注册[%s]", path))
+	}
+	root.handler = handleFunc
+}
+
+func (r *router) findRoute(method string, path string) (*node, bool) {
+	// 基本上是不是也是沿着树深度查找下去？
+	root, ok := r.trees[method]
+	if !ok {
+		return nil, false
+	}
+
+	if path == "/" {
+		return root, true
+	}
+
+	// 这里把前置和后置的 / 都去掉
+	path = strings.Trim(path, "/")
+
+	// 按照斜杠切割
+	segs := strings.Split(path, "/")
+	for _, seg := range segs {
+		child, found := root.childOf(seg)
+		if !found {
+			return nil, false
+		}
+		root = child
+	}
+	// 代表我确实有这个节点
+	// 但是节点是不是用户注册的有 handler 的，就不一定了
+	return root, true
+
+	// return root, root.handler != nil
+}
+
+func (n *node) childOrCreate(seg string) *node {
+	if n.children == nil {
+		n.children = map[string]*node{}
+	}
+	res, ok := n.children[seg]
+	if !ok {
+		// 要新建一个
+		res = &node{
+			path: seg,
+		}
+		n.children[seg] = res
+	}
+	return res
+}
+
+func (n *node) childOf(path string) (*node, bool) {
+	if n.children == nil {
+		return nil, false
+	}
+	child, ok := n.children[path]
+	return child, ok
+}
+
+// type tree struct {
+// 	root *node
+// }
+
+type node struct {
+	path string
+
+	// 子 path 到子节点的映射
+	children map[string]*node
+
+	// 缺一个代表用户注册的业务逻辑
+	handler HandleFunc
+}
